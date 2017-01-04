7e417b41c4fb7db66fe2de1713264c0b0c87738f
zeyzo <zeyzo@hotmail.com>
2017-01-04 12:37:56 +0300

    edit

---
+++ DB-Hospital/src/GUI.java
@@ -0,0 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTable;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(31, 208, 142, 27);
+		contentPane.add(comboBox);
+		
+		JButton btnView = new JButton("View");
+		btnView.setBounds(307, 207, 117, 29);
+		contentPane.add(btnView);
+		
+		table = new JTable();
+		table.setBounds(21, 6, 403, 190);
+		contentPane.add(table);
+		
+		JButton btnNewButton = new JButton("Search");
+		btnNewButton.setBounds(31, 243, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Insert");
+		btnNewButton_1.setBounds(176, 243, 117, 29);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Delete");
+		btnNewButton_2.setBounds(307, 243, 117, 29);
+		contentPane.add(btnNewButton_2);
+	}
+}
