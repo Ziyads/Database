2a4e43a818b15b9ab977f40cd3be70a877d8add1
zeyzo <zeyzo@hotmail.com>
2017-01-12 00:04:10 +0300

    many staff

--- DB-Hospital/src/DBConnect.java
+++ DB-Hospital/src/DBConnect.java
@@ -1,6 +1,9 @@
 import java.sql.*;
 import java.util.Scanner;
 
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 
 
 public class DBConnect {
@@ -249,6 +252,38 @@ public class DBConnect {
 		catch(Exception e){
 			System.out.println("Error:"+e);
 		}
+	}
+	public void FillTable(JTable table, String Query)
+	{
+	    try
+	    {
+	        //CreateConnection();
+	        Statement stat = con.createStatement();
+	        ResultSet rs = stat.executeQuery(Query);
+
+	        //To remove previously added rows
+	        while(table.getRowCount() > 0) 
+	        {
+	            ((DefaultTableModel) table.getModel()).removeRow(0);
+	        }
+	        int columns = rs.getMetaData().getColumnCount();
+	        while(rs.next())
+	        {  
+	            Object[] row = new Object[columns];
+	            for (int i = 1; i <= columns; i++)
+	            {  
+	                row[i - 1] = rs.getObject(i);
+	            }
+	            ((DefaultTableModel) table.getModel()).insertRow(rs.getRow()-1,row);
+	        }
+
+	        rs.close();
+	        stat.close();
+	        con.close();
+	    }
+	    catch(Exception e)
+	    {
+	    }
 	}
 	
 }
--- DB-Hospital/src/GUI.java
+++ DB-Hospital/src/GUI.java
@@ -2,10 +2,16 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JTable;
@@ -42,21 +48,70 @@ public class GUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(31, 208, 142, 27);
-		contentPane.add(comboBox);
 		
-		JButton btnView = new JButton("View");
-		btnView.setBounds(307, 207, 117, 29);
-		contentPane.add(btnView);
 		
 		table = new JTable();
 		table.setBounds(21, 6, 403, 190);
 		contentPane.add(table);
 		
-		JButton btnNewButton = new JButton("Search");
-		btnNewButton.setBounds(31, 243, 117, 29);
-		contentPane.add(btnNewButton);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(31, 208, 142, 27);
+		contentPane.add(comboBox);
+		String[] petStrings = { "Branch", "Department", "Doctor", "Paitient", "Appointment" };
+		comboBox.addItem(petStrings[0]);
+		comboBox.addItem(petStrings[1]);
+		comboBox.addItem(petStrings[2]);
+		comboBox.addItem(petStrings[3]);
+		comboBox.addItem(petStrings[4]);
+		comboBox.setSelectedIndex(0);
+	
+		
+		JButton btnView = new JButton("View");
+		btnView.setBounds(307, 207, 117, 29);
+		contentPane.add(btnView);
+		btnView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String s=e.getSource().toString().substring(277);
+				String z=s.substring(0, s.length()-1);
+				DBConnect DBC=new DBConnect();
+			
+
+				switch(z){
+					case "Branch":
+						 
+							
+						  
+						      
+						  
+						break;
+					case "Department":
+						
+						break;
+					case "Doctor":
+						
+						break;
+					case "Paitient":
+						
+						break;
+					case "Appointment":
+						
+						break;
+					default:
+						System.out.println("*");
+				}
+			}
+		});
+		
+		
+		JButton jb_search = new JButton("Search");
+		jb_search.setBounds(31, 243, 117, 29);
+		contentPane.add(jb_search);
+		jb_search.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				
+			}
+		});
 		
 		JButton jb_insert = new JButton("Insert");
 		jb_insert.setBounds(176, 243, 117, 29);
@@ -76,8 +131,17 @@ public class GUI extends JFrame {
 			}
 		});
 		
-		JButton btnNewButton_2 = new JButton("Delete");
-		btnNewButton_2.setBounds(307, 243, 117, 29);
-		contentPane.add(btnNewButton_2);
+		JButton jb_delete = new JButton("Delete");
+		jb_delete.setBounds(307, 243, 117, 29);
+		contentPane.add(jb_delete);
+		jb_delete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				
+			}
+		});
+		
+		
+		
 	}
+	
 }
